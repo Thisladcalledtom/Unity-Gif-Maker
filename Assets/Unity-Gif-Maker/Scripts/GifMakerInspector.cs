de53b046a19a0e227d5458a90e98d4721fe130bc
Thomas09 <tomironmonger24@gmail.com>
2021-09-19 12:49:41 +0100

    Initial Commit

---
+++ Assets/Unity-Gif-Maker/Scripts/GifMakerInspector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GifMakerInspector : Editor
+{
+    private bool PNGAttributesFoldout;
+    private bool GIFAttributesFoldout;
+
+    public override void OnInspectorGUI()
+    {
+        //draws the original properties established in `GeneratePictures`
+        DrawDefaultInspector();
+
+        GIFMaker nfts = (GIFMaker)target;
+
+        PNGAttributesFoldout = EditorGUILayout.Foldout(PNGAttributesFoldout, "PNG Generation");
+        GIFAttributesFoldout = EditorGUILayout.Foldout(GIFAttributesFoldout, "GIF Generation");
+
+        GUILayout.Space(20);
+
+        if (PNGAttributesFoldout)
+        {
+            if (GUILayout.Button("Generate PNG From Prefab"))
+            {
+                nfts.GeneratePictureFromPrefab();
+            }
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Generate PNG's From File Directory"))
+            {
+                nfts.GeneratePicturesFromFileDirectory();
+            }
+        }
+
+        if (GIFAttributesFoldout)
+        {
+            if (GUILayout.Button("Generate GIF From Prefab"))
+            {
+                nfts.GenerateGifFromPrefab();
+            }
+
+            GUILayout.Space(20);
+
+            if (GUILayout.Button("Generate GIFS From File Directory"))
+            {
+                nfts.GenerateGifsFromFileDirectory();
+            }
+        }
+    }
+}
